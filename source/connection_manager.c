e24e31d9bd0bde197c6055826e9784c38b979662
Ryan Carper <rccarper@amazon.com>
2020-05-13 10:48:07 -0400

    Changes for endpoint monitoring prototype

--- source/connection_manager.c
+++ source/connection_manager.c
@@ -131,6 +131,10 @@ struct aws_http_connection_manager {
      */
     void *shutdown_complete_user_data;
 
+    aws_http_connection_manager_on_connection_created_fn *on_connection_created_callback;
+
+    void *on_connection_created_user_data;
+
     /*
      * Controls access to all mutable state on the connection manager
      */
@@ -661,6 +665,8 @@ struct aws_http_connection_manager *aws_http_connection_manager_new(
     manager->bootstrap = options->bootstrap;
     manager->system_vtable = g_aws_http_connection_manager_default_system_vtable_ptr;
     manager->external_ref_count = 1;
+    manager->on_connection_created_callback = options->on_connection_created_callback;
+    manager->on_connection_created_user_data = options->on_connection_created_user_data;
     manager->shutdown_complete_callback = options->shutdown_complete_callback;
     manager->shutdown_complete_user_data = options->shutdown_complete_user_data;
     manager->enable_read_back_pressure = options->enable_read_back_pressure;
@@ -1015,6 +1021,11 @@ static void s_aws_http_connection_manager_on_connection_setup(
         if (!is_shutting_down) {
             /* We reserved enough room for max_connections, this should never fail */
             AWS_FATAL_ASSERT(aws_array_list_push_back(&manager->connections, &connection) == AWS_OP_SUCCESS);
+
+            if (manager->on_connection_created_callback) {
+                manager->on_connection_created_callback(connection, manager->on_connection_created_user_data);
+            }
+
         } else {
             /*
              * We won't add the connection to the pool; just release it immediately
